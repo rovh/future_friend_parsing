ffcefe70d8517971f09344ce4188787aa071efdf
unknown <alexei.kltln@gmail.com>
2024-12-31 15:55:12 +0300

    `

--- api/index.py
+++ api/index.py
@@ -24,12 +24,12 @@ def run_parsing():
 
     text = "00000"
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    browser = webdriver.Chrome(options=chrome_options)
     try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        browser = webdriver.Chrome(options=chrome_options)
         browser.get("https://www.google.com")
         text = ("Page title was '{}'".format(browser.title))
     finally:
